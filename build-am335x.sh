bd1e64177400f716cfb2deb9a350a7f6e11d0817
Merge With Me <178376848+MergeWithMe@users.noreply.github.com>
2025-03-28 02:07:49 +0100

    yalla

--- build-am335x.sh
+++ build-am335x.sh
@@ -5,7 +5,7 @@ sys=$(uname -m)
 # Required dependencies: bc bison flex libssl-dev u-boot-tools binutils gcc
 DIR="$PWD"
 
-configs=("am335x_evm_defconfig" "am335x_evm_defconfig_netboot")
+configs=("am335x_evm_defconfig" "am335x_evm_netboot_defconfig")
 
 if [ ! -f ./load.menuconfig ] ; then
     echo "Developers: to make changes: [touch load.menuconfig]"
@@ -30,6 +30,6 @@ for config in "${configs[@]}"; do
 done
 
 cp build_am335x_evm_defconfig/u-boot.img ../tftpboot/u-boot.img
-cp build_am335x_evm_defconfig_netboot/u-boot.img ../tftpboot/u-boot-restore.img
+cp build_am335x_evm_netboot_defconfig/u-boot.img ../tftpboot/u-boot-restore.img
 cp build_am335x_evm_defconfig/MLO ../tftpboot/MLO
-cp build_am335x_evm_defconfig_netboot/spl/u-boot-spl.bin ../tftpboot/u-boot-spl-restore.bin
+cp build_am335x_evm_netboot_defconfig/spl/u-boot-spl.bin ../tftpboot/u-boot-spl-restore.bin
